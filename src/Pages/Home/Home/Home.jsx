f819db558b869a69c419bec686f7c7754842e5cc
ashanur00 <paglacoc00@gmail.com>
2025-04-12 18:52:24 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import MarQuee from "../../../Component/Marquee/MarQuee";
+import Serach from "../../../Component/Search/Search/Serach";
+
+const Home = () => {
+    return (
+        <div>
+            <MarQuee/>
+            <Serach/>
+            <h1 className="text-3xl text-center mb-5">নাগরিক আবেদন</h1>
+            
+        </div>
+    );
+};
+
+export default Home;
